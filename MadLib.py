15cab72319b422424ee24b7e3503281dd8578062
Alex <AlexSteele79@gmail.com>
2020-03-21 06:45:50 -0400

    Finished word_list
    Completed the generate_word_list function, tested the regex

--- MadLib.py
+++ MadLib.py
@@ -1,23 +1,30 @@
+from madlib_stories import stories
+import random
+import re
 
-def MadLib:
+class MadLib:
     # Should have a text associated with it
     text = None
     # Should have a generated list of required words in the NOUN, ADJECTIVE, VERB format
     word_list = None
 
     def __init__(self):
-        # Upon initialization it should generate the list of required words
         # Also during initialization it should replace the action words in self.text with {} so that
             # it can be .formatted()
-        self.text = select_random_madlib()
-        self.word_list = generate_required_words(self.text)
-        self.text = prime_text_for_formatting(self.text)
+        self.text = random.choice(stories)
+        # self.word_list = generate_required_words(self.text)
+        self.word_list = generate_required_words()
+        # self.text = prime_text_for_formatting(self.text)
 
-    def select_random_madlib(self):
-        # Select a random madlib
+    def generate_required_words(self):
+        search_pattern = re.compile(r'VERB | ADJECTIVE | NOUN | NAME', re.VERBOSE)
+        word_list = search_pattern.findall(self.text)
 
-    def generate_required_words(madlib):
-        # Generates and returns the required word list
+        return word_list
 
     def prime_text_for_formatting(unformatted_madlib):
         # Remove all required word types and replace with {}
+
+
+newLib = MadLib()
+print(newLib.word_list)
