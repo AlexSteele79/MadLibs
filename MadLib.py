4e4f47fe2997d9d12bfc4a0b64860560d0195fc7
Alex <AlexSteele79@gmail.com>
2020-03-21 05:54:04 -0400

    Madlib_stories creation
    Created Madlib_stories to test everything with

--- MadLib.py
+++ MadLib.py
@@ -3,13 +3,21 @@ def MadLib:
     # Should have a text associated with it
     text = None
     # Should have a generated list of required words in the NOUN, ADJECTIVE, VERB format
+    word_list = None
 
     def __init__(self):
         # Upon initialization it should generate the list of required words
         # Also during initialization it should replace the action words in self.text with {} so that
             # it can be .formatted()
         self.text = select_random_madlib()
+        self.word_list = generate_required_words(self.text)
+        self.text = prime_text_for_formatting(self.text)
 
     def select_random_madlib(self):
         # Select a random madlib
-    def generateRequiredWords(madlib):
+
+    def generate_required_words(madlib):
+        # Generates and returns the required word list
+
+    def prime_text_for_formatting(unformatted_madlib):
+        # Remove all required word types and replace with {}
---
+++ madlib_stories.py
@@ -0,0 +1,10 @@
+stories = ["""Once upon a time in the land of NOUN,
+there lived a prince whose name was NAME.
+He was ADJECTIVE, certainly, but he had ADJECTIVE hair.
+On the day he was to become king he had to VERB to the
+local NOUN. On his way there he discovered a NOUN which
+made him rethink monarchies. Determined to change it he VERBed
+back. But his intentions were too ADJECTIVE - it was a surprise
+crowning. Instead of elected officials, he would forever be known
+as the ADJECTIVE king!"""
+]
