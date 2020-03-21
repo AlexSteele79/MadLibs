dc099818d9b9afc99d67c949e1c46c854c088080
Alex <AlexSteele79@gmail.com>
2020-03-21 07:09:07 -0400

    Completed core functionality
    MadLib class can now:
    - Choose a random MadLib
    - Generate the required word list
    - Reformat the string to make it easier to fill in the blanks

--- MadLib.py
+++ MadLib.py
@@ -2,29 +2,21 @@ from madlib_stories import stories
 import random
 import re
 
+
 class MadLib:
-    # Should have a text associated with it
+    pattern = re.compile(r'VERB | ADJECTIVE | NOUN | NAME', re.VERBOSE)
+
     text = None
-    # Should have a generated list of required words in the NOUN, ADJECTIVE, VERB format
     word_list = None
 
     def __init__(self):
-        # Also during initialization it should replace the action words in self.text with {} so that
-            # it can be .formatted()
         self.text = random.choice(stories)
-        # self.word_list = generate_required_words(self.text)
-        self.word_list = generate_required_words()
-        # self.text = prime_text_for_formatting(self.text)
+        self.word_list = self.generate_required_words()
+        self.prime_text_for_formatting()
 
     def generate_required_words(self):
-        search_pattern = re.compile(r'VERB | ADJECTIVE | NOUN | NAME', re.VERBOSE)
-        word_list = search_pattern.findall(self.text)
-
+        word_list = self.pattern.findall(self.text)
         return word_list
 
-    def prime_text_for_formatting(unformatted_madlib):
-        # Remove all required word types and replace with {}
-
-
-newLib = MadLib()
-print(newLib.word_list)
+    def prime_text_for_formatting(self):
+        self.text = re.sub(self.pattern, "{}", self.text)
