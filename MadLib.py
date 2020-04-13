31e87d280c772edcbaaba504650bbd2d4f3c0182
Alex <AlexSteele79@gmail.com>
2020-04-13 15:42:27 -0400

    Added Pipeline class
    Pipeline class has most methods needed for funtionality.

--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,29 @@
 from madlib_stories import stories
+<<<<<<< Updated upstream
+=======
+from Pipeline import Pipeline
+from Story import Story
+
+import os
+>>>>>>> Stashed changes
 import random
 import re
-import os
+import spacy
 
 
 class MadLib:
+<<<<<<< Updated upstream
     pattern = re.compile(r'VERB | ADJECTIVE | NOUN | NAME', re.VERBOSE)
 
     madlib_story = None
     word_requirement_list = []
     word_list = []
     filled_word_list = []
+=======
+    madlib_story = Story().corpus
+    pipeline = Pipeline(madlib_story)
+
+>>>>>>> Stashed changes
 
     def __init__(self):
         self.madlib_story = random.choice(stories)
@@ -19,10 +32,13 @@ class MadLib:
         self.prompt_user_input()
         self.show_complete_madlib()
 
+<<<<<<< Updated upstream
     def generate_required_words(self):
         word_list = self.pattern.findall(self.madlib_story)
         return word_list
 
+=======
+>>>>>>> Stashed changes
     def prime_text_for_formatting(self):
         self.madlib_story = re.sub(self.pattern, "%s", self.madlib_story)
 
@@ -33,5 +49,9 @@ class MadLib:
 
     def show_complete_madlib(self):
         tuple_list = tuple(self.word_list)
+<<<<<<< Updated upstream
         print(self.madlib_story % tuple_list)
 
+=======
+        # print(self.madlib_story % tuple_list)
+>>>>>>> Stashed changes
---
+++ Story.py
@@ -0,0 +1,8 @@
+class Story:
+	corpus = None
+
+	def __init__(self):
+		self.corpus = "George was a happy King. George was also the only King."
+
+	# def get_text(self):
+	# 	print(self.corpus)
