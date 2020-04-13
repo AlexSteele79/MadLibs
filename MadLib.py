4d0722e4243d9d4764bf48266f03d5976f4613b0
Alex <AlexSteele79@gmail.com>
2020-04-13 15:51:51 -0400

    Added Pipeline class
    Pipeline class has most methods needed for funtionality.

--- MadLib.py
+++ MadLib.py
@@ -1,57 +1,35 @@
 from madlib_stories import stories
-<<<<<<< Updated upstream
-=======
 from Pipeline import Pipeline
 from Story import Story
 
-import os
->>>>>>> Stashed changes
 import random
-import re
 import spacy
 
 
 class MadLib:
-<<<<<<< Updated upstream
-    pattern = re.compile(r'VERB | ADJECTIVE | NOUN | NAME', re.VERBOSE)
+    # Take in the class from Pipeline
+    # Randomly choose words to change
+    # Pass to input class
+    word_tag_list = None
+    word_locations = None
 
-    madlib_story = None
-    word_requirement_list = []
-    word_list = []
-    filled_word_list = []
-=======
-    madlib_story = Story().corpus
-    pipeline = Pipeline(madlib_story)
-
->>>>>>> Stashed changes
-
-    def __init__(self):
+    def __init__(self, word_tag_list, word_locations):
+        self.word_tag_list = word_tag_list
         self.madlib_story = random.choice(stories)
         self.word_requirement_list = self.generate_required_words()
         self.prime_text_for_formatting()
         self.prompt_user_input()
         self.show_complete_madlib()
 
-<<<<<<< Updated upstream
+    def replace_proper_nouns(self, pnoun):
+        for index in self.word_locations[pnoun]:
+            self.word_tag_list[index][0] = "{}"
+
     def generate_required_words(self):
         word_list = self.pattern.findall(self.madlib_story)
         return word_list
-
-=======
->>>>>>> Stashed changes
-    def prime_text_for_formatting(self):
-        self.madlib_story = re.sub(self.pattern, "%s", self.madlib_story)
 
     def prompt_user_input(self):
         for word in self.word_requirement_list:
             user_word = input("Please enter a {}: ".format(word))
             self.word_list.append(user_word)
-
-    def show_complete_madlib(self):
-        tuple_list = tuple(self.word_list)
-<<<<<<< Updated upstream
-        print(self.madlib_story % tuple_list)
-
-=======
-        # print(self.madlib_story % tuple_list)
->>>>>>> Stashed changes
